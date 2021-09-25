0a53616478d1db2f15f0ab8acf799d7ab250725c
pranay202 <pranaykharabe20@gmail.com>
2021-09-25 23:22:26 +0530

    Initial Commit

---
+++ server/Database/restaurant/index.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const RestaurantSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    city: {type: String, required: true},
+    address: {type: String, required: true},
+    mapLocation: {type: String, required: true},
+    cuisine: [String],
+    restaurantTimings: String,
+    contactNumber: Number,
+    website: String,
+    popularDishes: [String],
+    averageCost: Number,
+    amenties: [String],
+    menuImages: {
+        type: mongoose.Types.ObjectId,
+        ref: "Images"
+    },
+    menu: {
+        type: mongoose.Types.ObjectId,
+        ref: "Menu"
+    },
+    reviews: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Reviews"
+    }],
+    photos: {
+        type: mongoose.Types.ObjectId,
+        ref: "Images"
+    }
+},
+{
+    timestamps: true
+})
---
+++ server/Database/reviews/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const ReviewSchema = mongoose.Schema({
+    food: {type: mongoose.Types.ObjectId, ref:"Foods"},
+    user: {type: mongoose.Types.ObjectId, ref:"Users"},
+    restaurant: {type: mongoose.Types.ObjectId, ref:"Restaurants"},
+    rating: {type: Number, required: true},
+    reviewText:{type: String, required: true},
+    photos: [{type: mongoose.Types.ObjectId, ref: "Images"}]
+},
+{
+    timestamps: true
+});
+
+export const ReviewModel = mongoose.model("Reviews", ReviewSchema);
