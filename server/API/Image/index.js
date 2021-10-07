3759a80041600a184d84a99c05a5114a8cf3d6bc
pranay202 <pranaykharabe20@gmail.com>
2021-10-07 13:57:06 +0530

    added Img-API by AWS-S3upload

---
+++ server/API/Image/index.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import AWS from 'aws-sdk';
+import multer from 'multer';
+
+//Database Model
+import { ImageModel } from "../../Database/allModels";
+
+const Router = express.Router();
+
+//config multer
+const storage = multer.memoryStorage();
+const upload = multer({storage});
+
+//AWS S3 bucket config
+// const s3Bucket = new AWS.S3({
+//     accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+//     secretAccessKey: process.env.AWS_S3_SECRET_KEY,
+//     region: "ap-south-1"
+// });
+
+
+/* 
+Route               /
+Description         Uploading given image to S3 bucket, and then saving to MongoDB database
+Params              None
+Access              Public
+Method              POST
+*/
+
+Router.post("/", async (req, res) => {
+    try {
+        const file = req.file;
+        
+        //S3 Bucket Options
+        const bucketOptions = {
+            Bucket: "",
+            Key: file.originalname,
+            Body: file.buffer,
+            ContentType: file.mimetype,
+            ACL: "public-read"
+        };
+        
+        // const s3Upload = (options) => {
+        //     return new Promise((resolve, reject) => {
+        //         s3Bucket.upload(options, (err, data) => {
+        //             if (error) return reject(error);
+        //             return resolve(data);
+        //         })
+        //     })
+        // };
+        
+        // const uploadImage = await s3Upload(bucketOptions);
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+export default Router;
