f4d22330582f91b2b034ccfe68773f8aadba940f
pranay202 <pranaykharabe20@gmail.com>
2021-10-07 13:56:02 +0530

    added Food API

---
+++ server/API/Food/index.js
@@ -0,0 +1,50 @@
+// Libraries
+import express from 'express';
+// import passport from 'passport';
+
+//Database Model
+import { FoodModel } from '../../Database/allModels';
+
+const Router = express.Router();
+
+/*
+Route                     /
+Description               Get all the food based on  a particular restaurant
+Params                    _id
+Access                    Public
+Method                    Get
+*/
+
+Router.get("/:_id", async (req, res) => {
+    try {
+        const { _id } = req.params;
+        const foods = await FoodModel.find({ restaurant: _id });
+        
+        return res.json({ foods });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+/*
+Route                     /r
+Description               Get all the foods based on particular category
+Params                    category
+Access                    Public
+Method                    Get
+*/
+
+Router.get("/r/:category", async (req, res) => {
+    try {
+        const {category} = req.params;
+        const foods = await FoodModel.find({
+            category: { $regex: category, $options: "i" }
+        });
+        
+        return res.json({ foods });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+export default Router;
